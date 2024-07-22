ea26b9ce4ccafe82d7ffca376cf751abdbc0d6e3
Suraj M Durgad <surajmdurgad@gmail.com>
2024-07-22 23:43:37 +0530

    Reorderd the component

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -2,8 +2,8 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { Modal, Text, View } from 'react-native';
 
 import Styles from './styles';
-import Header from '../Header';
-import List from '../List';
+import Header from './Header';
+import List from './List';
 
 const Home = () => {
   return (
